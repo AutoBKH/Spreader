0062cd7ad08f9224fdd82ae8d2d1d16e57ba2373
shani hochma <shanihochma@gmail.com>
2020-04-01 15:34:26 +0300

    start working on postgres setup

--- setupEnvironment/build_spreader_ecosystem.sh
+++ setupEnvironment/build_spreader_ecosystem.sh
@@ -7,6 +7,10 @@ docker container create --name spreading_handler spreading_handler.image
 
 # build rabbit mq container
 docker load -i setupEnvironment/dockerImages/rabbitmq_management.image
-docker build -t extended_rabbitmq_manageme.gitignorent.image -f setupEnvironment/dockerfiles/extended_rabbit_mq.dockerfile .
+docker build -t extended_rabbitmq_management.image -f setupEnvironment/dockerfiles/extended_rabbit_mq.dockerfile .
 docker container create -p 5672:5672 -p 15672:15672 --name some-rabbit extended_rabbitmq_management.image
 
+# build postgres container
+docker load -i setupEnvironment/dockerImages/postgres.image
+docker build -t extended_postgres.image -f setupEnvironment/dockerfiles/extended_postgres.dockerfile .
+docker container create -p 5432:5432 --name some-postgres extended_postgres.image
